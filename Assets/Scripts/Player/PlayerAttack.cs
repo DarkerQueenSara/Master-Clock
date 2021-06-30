3bcb74dcc1bd4bd6eabf10bc31814cd07b2573a2
HerouFenix <diogo04@ua.pt>
2021-06-30 16:07:33 +0100

    player slash

--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -4,5 +4,18 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
+    public LayerMask enemyLayer;
+
     public int attackDamage = 1;
+
+    /*
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Swoosh");
+        if(collision.gameObject.layer == enemyLayer) // If collided with an enemy
+        {
+            Debug.Log("Hit enemy!");
+        }
+    }
+    */
 }
